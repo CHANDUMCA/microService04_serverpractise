2ed34c74f2cf1a81353e64847da898dde94c3e67
CHANDUMCA <chandumca234@gmail.com>
2026-01-02 08:23:29 +0530

    Initial commit of Spring Boot project

---
+++ src/main/java/com/sunrize/it/MicroService04ServerpractiseApplication.java
@@ -0,0 +1,13 @@
+package com.sunrize.it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroService04ServerpractiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroService04ServerpractiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunrize/it/MicroService04ServerpractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunrize.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroService04ServerpractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
