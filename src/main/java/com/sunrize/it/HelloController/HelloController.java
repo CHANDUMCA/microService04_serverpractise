a922060f83928c74ba97e7192e91024cd9ae3ed9
CHANDUMCA <chandumca234@gmail.com>
2026-01-02 08:48:56 +0530

    delete application.properties file created new yaml file,updated contrler

--- src/main/java/com/sunrize/it/HelloController/HelloController.java
+++ src/main/java/com/sunrize/it/HelloController/HelloController.java
@@ -1,13 +1,18 @@
 package com.sunrize.it.HelloController;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+	@Value("${app.message}")
+	private String message;
+
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello from Spring Boot";
+		return message;
 	}
 
 }
